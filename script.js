023eebeb2505a00362632a48306b287a30f12fbb
dasha <dashka.didyk@gmail.com>
2020-07-02 20:32:36 +0300

    part1

---
+++ script.js
@@ -0,0 +1,22 @@
+// let userIsHappy = true;
+// let number = 0;
+
+// do {
+//     userIsHappy = confirm("Are you happy that the number is " + ++number + "?");
+// } while (userIsHappy);
+
+// let box = document.querySelector(".box");
+// console.log("box");
+// let moveBy = 200;
+// let moved = parseInt(box.style.marginLeft) || 0;
+// console.log("marg")
+
+// do {
+//     box.style.marginLeft = (++moved +"px");
+// } while (moved < moveBy);
+
+//  while (moved < moveBy) box.style.marginLeft = (++moved +"px");
+
+for (let number = 1, isUserHappy = true; isUserHappy; number++) {
+    isUserHappy = confirm("Are you happy that the number is " + number + "?");
+}
